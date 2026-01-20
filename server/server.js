758abe39d65ac898e5ae9fa1c6efe00f167c3a21
SamratTheProgrammer <samratsahaonline@gmail.com>
2026-01-21 00:25:54 +0530

    Wbhooks added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import { connect } from 'mongoose'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 // App Config
 const PORT = process.env.PORT || 4000
@@ -14,5 +15,7 @@ app.use(cors())
 
 // API routes
 app.get('/', (req, res) => res.send("API Working"))
+
+app.use('/api/user',userRouter)
 
 app.listen(PORT, () => console.log("Server Running on port " + PORT))
